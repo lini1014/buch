13bc8cfdd40c4238d164bfc4e9205cc9f87f46bd
lini1014 <lini1014@h-ka.de>
2025-12-19 14:31:07 +0100

    types.ts hinzugefügt

---
+++ frontend/src/lib/types.ts
@@ -0,0 +1,37 @@
+export type Buchart = 'EPUB' | 'HARDCOVER' | 'PAPERBACK';
+
+export type Titel = {
+    readonly titel: string;
+    readonly untertitel?: string | null;
+};
+
+export type Abbildung = {
+    readonly beschriftung?: string | null;
+    readonly contentType?: string | null;
+};
+
+export type Buch = {
+    readonly id: number;
+    readonly version?: number;
+    readonly isbn: string;
+    readonly rating: number;
+    readonly art?: Buchart | null;
+    readonly preis?: number | null;
+    readonly rabatt?: number | null;
+    readonly lieferbar?: boolean | null;
+    readonly datum?: string | null;
+    readonly homepage?: string | null;
+    readonly schlagwoerter?: string[] | null;
+    readonly titel: Titel;
+    readonly abbildungen?: Abbildung[] | null;
+};
+
+export type Page<T> = {
+    readonly content: T[];
+    readonly page: {
+        readonly size: number;
+        readonly number: number;
+        readonly totalElements: number;
+        readonly totalPages: number;
+    };
+};
