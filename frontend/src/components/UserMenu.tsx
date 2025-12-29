1e6109fb72cd6088e542ead898a0a6ab249d36af
enescucu1 <enes.furkan.cubukcu@gmail.com>
2025-12-29 18:49:48 +0100

    UserMenu: Menü-Logik ergänzt

--- frontend/src/components/UserMenu.tsx
+++ frontend/src/components/UserMenu.tsx
@@ -1,5 +1,6 @@
 import MenuRoundedIcon from '@mui/icons-material/MenuRounded';
-import { IconButton } from '@mui/material';
+import { IconButton, Menu } from '@mui/material';
+import { useState } from 'react';
 
 type Props = {
     username?: string | null;
@@ -7,18 +8,31 @@ type Props = {
 };
 
 export function UserMenu({}: Props) {
+    const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
+
     return (
-        <IconButton
-            sx={{
-                position: 'absolute',
-                top: 16,
-                right: 16,
-                color: '#d32f2f',
-                bgcolor: 'rgba(255,255,255,0.06)',
-                '&:hover': { bgcolor: 'rgba(255,255,255,0.12)' },
-            }}
-        >
-            <MenuRoundedIcon />
-        </IconButton>
+        <>
+            <IconButton
+                onClick={(event) => setMenuAnchor(event.currentTarget)}
+                sx={{
+                    position: 'absolute',
+                    top: 16,
+                    right: 16,
+                    color: '#d32f2f',
+                    bgcolor: 'rgba(255,255,255,0.06)',
+                    '&:hover': { bgcolor: 'rgba(255,255,255,0.12)' },
+                }}
+            >
+                <MenuRoundedIcon />
+            </IconButton>
+
+            <Menu
+                anchorEl={menuAnchor}
+                open={Boolean(menuAnchor)}
+                onClose={() => setMenuAnchor(null)}
+                anchorOrigin={{ vertical: 'bottom', horizontal: 'right' }}
+                transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+            />
+        </>
     );
 }
